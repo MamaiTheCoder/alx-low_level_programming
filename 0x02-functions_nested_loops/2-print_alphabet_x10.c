dd43bf5987b9a94bb4dce51040ee570a8344b627
MamaiTheCoder <jmakoks@gmail.com>
2022-09-13 00:17:31 -0700

    Check for alphabets

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,12 +10,12 @@ void print_alphabet_x10(void)
 {
 	int x, y;
 
-	for (x = 0; x < 9; x++)
+	for (x = 0; x < 10; x++)
 	{
 		for (y = 97; y <= 122; y++)
 		{
 			_putchar(y);
 		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }

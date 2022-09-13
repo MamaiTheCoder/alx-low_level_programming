d1ffd3f8904deb6202254f1f9cbaba49a385ea8f
MamaiTheCoder <jmakoks@gmail.com>
2022-09-13 02:29:03 -0700

    Adding 9 times code

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * times_table - prints 9 times table
+ *
+ * Return: no return
+ */
+void times_table(void)
+{
+	int num, mult, prod;
+
+	for (num = 0; num <= 9; ++num)
+	{
+		_putchar(48);
+		for (mult = 1; mult <= 9; ++mult)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			prod = num * mult;
+
+			/*
+			 * if the product is a single number put space
+			 * if it is two number place the first digit
+			 */
+			if (prod <= 9)
+				_putchar(' ');
+			else
+				_putchar((prod % 10) + 48);/*get the first digit*/
+			_putchar((prod % 10) + 48);/*get second digit*/
+		}
+		_putchar('\n');
+	}
+
+}

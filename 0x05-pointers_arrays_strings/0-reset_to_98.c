779f5814ee89cd5948c1f10783f8edcced58b697
MamaiTheCoder <jmakoks@gmail.com>
2022-09-18 22:29:04 -0700

    Testing my file

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - update the value it points to 98
+ *
+ * @*n - The character
+ *
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	int a = 98;
+	int *n;
+
+	n = &a;
+}

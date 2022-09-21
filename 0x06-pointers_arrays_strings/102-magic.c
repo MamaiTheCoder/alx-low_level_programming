9418f3a4aaa73dd5ed4e81a73172600c2104c19c
MamaiTheCoder <jmakoks@gmail.com>
2022-09-21 09:27:46 -0700

    Editting the lines of code

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -5,11 +5,11 @@ int main(void)
 	int n;
 	int a[5];
 	int *p;
-
+	
 	a[2] = 1024;
 	p = &n;
-	/**
-	 * write you line of code here...
+	/*
+	 * write your line of code here...
 	 * Remember:
 	 * - you are not allowed to use a
 	 * - you are not allowed to modify p
@@ -17,7 +17,7 @@ int main(void)
 	 * - you are not allowed to code anything else than this line of code
 	 */
 	;
-	/*...so that this prints 98\n*/
+	/* ...so that this prints 98\n */
 	*(p + 5) = 98;
 	printf("a[2] = %d\n", a[2]);
 	return (0);

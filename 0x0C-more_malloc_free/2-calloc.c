f4cd0fadbc82d87f48d7159a454950bacadab323
MamaiTheCoder <jmakoks@gmail.com>
2022-10-06 11:06:59 -0700

    Using malloc to allcate memory '

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - allocates memory for an array.
+ *
+ * @nmemb: number of elements.
+ * @size: of bytes.
+ *
+ * Return: pointer to allocated memory.
+ * NULL if nmemb or size is 0.
+ * NuLL if malloc fails.
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *p;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	p = malloc(nmemb * size);
+
+	if (p == NULL)
+		return (NULL);
+
+	for (i = 0; i < (nmemb * size); i++)
+		p[i] = 0;
+	return (p);
+}

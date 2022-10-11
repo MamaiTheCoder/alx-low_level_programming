25df1e0fd51babe9285a987cdc4cb9ff9c6833a8
MamaiTheCoder <jmakoks@gmail.com>
2022-10-11 05:22:54 -0700

    Rearranging prototypes

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,6 +2,10 @@
 #define DOG_H
 
 /**
+ * dog_t - typed struct dog
+ */
+typedef struct dog dog_t;
+/**
  * stuct dog - keeps information of the dog.
  *
  * @name: name of the dog
@@ -19,11 +23,6 @@ struct dog
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-
-/**
- * dog_t - typed struct dog
- */
-typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 

6044f71a5685dee3aeb7ce38137b36661bff648f
Emmanuel Makokha <99004575+MamaiTheCoder@users.noreply.github.com>
2022-10-27 21:43:03 +0300

    Update 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,13 +9,13 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t number_of_nodes = 0;
+	size_t count = 0;
 
 	while (h != NULL)
 	{
-		number_of_nodes++;
 		printf("%d\n", h->n);
 		h = h->next;
+		count++;
 	}
-	return (number_of_nodes);
+	return (count);
 }

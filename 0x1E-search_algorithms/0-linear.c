ee40a60a32fbe7dfadc6ea2333ba25cc6c3d7e13
Emmanuel Makokha <99004575+MamaiTheCoder@users.noreply.github.com>
2023-04-13 15:33:04 +0300

    Update condition

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -17,9 +17,9 @@ int linear_search(int *array, size_t size, int value)
 	if (array == NULL || size == 0)
 		return (-1);
 
-	for (i = 0; i < size; i++)
+	for (i = 0; i < (int)size; i++)
 	{
-		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
 		if (value == array[i])
 		{
 			return (i);
